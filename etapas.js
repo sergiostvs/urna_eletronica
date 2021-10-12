6cc977ccdc3743c0c70dbd006c8bb5d3b1bf0a0f
Sergio Henrique <sergiohme@gmail.com>
2021-10-12 09:46:34 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '53222',
+                nome: 'Nome do Candidato 1',
+                partido: 'PPP',
+                fotos: [
+                    {url:'astronauta.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '85444',
+                nome: 'Nome do Candidato 2',
+                partido: 'AAA',
+                fotos: [
+                    {url:'bombeira.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '53',
+                nome: 'Nome do Candidato 1',
+                partido: 'PPP',
+                fotos: [
+                    {url:'pintor.png', legenda: 'Prefeito'},
+                    {url:'enfermeira.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '85',
+                nome: 'Nome do Candidato 2',
+                partido: 'AAA',
+                fotos: [
+                    {url:'boto.png', legenda: 'Prefeito'},
+                    {url:'detetive.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
